1af263654676be8e01aa32ebc04b33b149e46f8e
LM-Abapo <lyza2067@gmail.com>
2025-08-29 23:46:13 +0800

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string name;
+    int num1, num2, sum;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << "! Let's add two numbers." << endl;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << endl;
+
+    return 0;
+}
+
